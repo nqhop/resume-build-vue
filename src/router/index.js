8e675d13191c282b2e85f5ddcb9257dd06d636fa
Nguyễn Quốc Hợp <hop35889@gmail.com>
2023-12-15 15:21:36 +0700

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 // Composables
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from "../components/pages/dashboard/Dashboard.vue";
-import Template0 from "../components/pages/template/template0.vue";
+import Template0 from "../components/pages/template/TemplateForResume.vue";
 import CreateResume from "../components/pages/resume/CreateResume.vue";
 import TemplateForResume2 from "../components/pages/template/templateForResume2.vue";
 import TemplateForResume3 from "../components/pages/template/templateForResume3.vue";
@@ -9,17 +9,10 @@ import TemplateForResume3 from "../components/pages/template/templateForResume3.
 const routes = [
   { path: "/", redirect: "/dashboard" },
   { path: "/dashboard", component: Dashboard, name: "dashboard" },
-<<<<<<< HEAD
   { path: "/template", component: Template0, name: "template" },
   { path: "/resume", component: CreateResume, name: "resume" },
-  { path: "/template2", component: Template2, name: "template2" },
-  { path: "/Template3", component: Template3, name: "Template3" },
-=======
-  // { path: "/template", component: Template0, name: "template" },
-  // { path: "/resume", component: CreateResume, name: "resume" },
-  // { path: "/template2", component: TemplateForResume2, name: "template2" },
-  // { path: "/Template3", component: TemplateForResume3, name: "Template3" },
->>>>>>> 000516ae1098538e04e362a60d493b924e17a6ff
+  { path: "/template2", component: TemplateForResume2, name: "template2" },
+  { path: "/Template3", component: TemplateForResume3, name: "Template3" },
 ];
 
 const router = createRouter({
